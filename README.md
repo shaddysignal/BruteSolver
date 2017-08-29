a3a7aa08dc05fd33c9b029039102b8668827181c
Anton Shubin <signal2564@gmail.com>
2017-08-29 19:41:07 +0300

    change readme, add solve_and_draw, some customisation of solve call

--- README.md
+++ README.md
@@ -6,4 +6,26 @@ Should always place tactical spaces when providing equation. Like x + 1 instead 
 
 ## Variants
 
-Right now its only 2 brutes. Completly random and intelligent one.
+Right now its only 2 brutes. Completely random and intelligent one.
+
+## Usage
+
+Very poor right now, but at least something.
+
+Do this: `mix do deps.get, deps.compile`
+then `iex -S mix`
+and then `BruteSolver.solve(IntelligentBrute, "x ^ 2 + y ^ 2 - 1")`. So sad though that it bad example.
+But `BruteSolver.solve(RandomBrute, "x ^ 2 + y ^ 2 - 1")` is much worse.
+
+Also you can do this: `BruteSolver.solve_and_draw(IntelligentBrute, "x ^ 2 + y ^ 2 - 1")`, which result in a drawing.
+The name of which will be "#{notation}.png". And it will be containing process of selecting possible solutions, but only
+after complete halt of the brute.
+
+## TODO
+
+- [ ] What is that thing they call `Behavior`?
+- [ ] What is that things they call `Supervisor` and `GenServer`?
+- [ ] Why is that simply one threaded? Should you probably make the tree of brutes that have each only little block and that crunch for solution in it?
+- [ ] Why even the most intelligent brute could not solve "x ^ 2 + y ^ 2"? Maybe you should use gradient slide(is that what it called?).
+- [ ] Maybe should add more options, like limits and such. But on the other hand, when it is wrapped in GenServer, do I really need it?
+
--- lib/brute_solver.ex
+++ lib/brute_solver.ex
@@ -16,12 +16,60 @@ defmodule BruteSolver do
       end)
   end
 
-  def solve(string) do
+  def solve(brute, string) do
     %Persar{notation: notaiton, variables: vars} = Persar.do_some(string)
 
-    {:ok, [solution | history]} =
-      IntelligentBrute.init(notaiton, vars |> Enum.reduce([], fn _, acc -> [{-1000, 1000} | acc] end), 0.0005, vars)
+    {:ok, [solution | _]} =
+      brute.init(notaiton, vars |> Enum.reduce([], fn _, acc -> [{-1000, 1000} | acc] end), 0.0005, vars)
 
     solution
   end
+
+  @doc """
+  Probably should be only 2 variables. But if one, then second assumed 0, if more then two, then they thrown away
+  """
+  def solve_and_draw(brute, string) do
+    radius = 10
+    dimension = 2000
+    half_dimension = div(dimension, 2)
+    strokeColor = "black"
+    historyColor = "RGBA(0, 255, 255, 0.5)"
+    solutionColor = "RGBA(255, 0, 0, 1)"
+
+    %Persar{notation: notaiton, variables: vars} = Persar.do_some(string)
+
+    {:ok, [solution | history]} =
+      brute.init(
+        notaiton,
+        vars |> Enum.reduce([], fn _, acc -> [{-half_dimension, half_dimension} | acc] end), 0.0005, vars)
+
+    circles = history |>
+      Enum.map(&(transform_point(&1, radius))) |>
+      Enum.map(fn {x, y, r} -> {x + half_dimension, y + half_dimension, r} end) |>
+      Enum.reverse
+
+    name = notaiton |> Enum.reduce("", fn e, acc -> "#{acc}#{e}" end)
+
+    temp = %Mogrify.Image{path: "#{name}.png", ext: "png"} |>
+      Mogrify.custom("size", "#{dimension}x#{dimension}") |>
+      Mogrify.canvas("none") |>
+      Mogrify.custom("fill", historyColor) |>
+      Mogrify.custom("stroke", strokeColor) |>
+      Mogrify.custom("strokewidth", 2)
+
+    temp = circles |> Enum.reduce(temp, fn {x, y, radius}, acc -> acc |> Mogrify.Draw.circle(x, y, x, y + radius) end)
+
+    [sx, sy | _] = solution |> Enum.map(&(&1 + half_dimension))
+
+    image = temp |>
+      Mogrify.custom("fill", solutionColor) |>
+      Mogrify.Draw.circle(sx, sy, sx, sy + radius) |>
+      Mogrify.create(path: ".")
+
+    {:ok, IO.inspect(image)}
+  end
+
+  defp transform_point([x, y | _], radius), do: {x, y, radius}
+  defp transform_point([x | tail], radius), do: transform_point([x, 0 | tail], radius)
+
 end
